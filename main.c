874ff97ab82a6d4c016e9dab5045037428ba36b0
Han-g <ark9854@naver.com>
2022-06-02 08:58:17 +0900

    6-1

--- main.c
+++ main.c
@@ -38,6 +38,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
 	WNDCLASSEX WndClass;
 	g_hInst = hInstance;
 
+	int FrameX = GetSystemMetrics(SM_CXFRAME), FrameY = GetSystemMetrics(SM_CYFRAME), Caption = GetSystemMetrics(SM_CYCAPTION);
+
 	WndClass.cbSize = sizeof(WndClass);
 	WndClass.style = CS_HREDRAW | CS_VREDRAW;
 	WndClass.lpfnWndProc = (WNDPROC)WndProc;
@@ -52,7 +54,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
 	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
 	RegisterClassEx(&WndClass);
-	hWnd = CreateWindow(lpszClass, lpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, window_size_w, window_size_d, NULL, (HMENU)NULL, hInstance, NULL);
+	box window = { 0,0,800,800 };
+	AdjustWindowRect(&window, WS_OVERLAPPEDWINDOW, FALSE);
+	hWnd = CreateWindow(lpszClass, lpszWindowName, CW_USEDEFAULT, 100, 100, window_size_w, window_size_d, NULL, (HMENU)NULL, hInstance, NULL);
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
@@ -71,7 +75,7 @@ typedef struct CHARACTER {
 	int speed, num_bubble, bubble_len;
 }Character;
 typedef struct BUBBLE {
-	int x, y, len, time;
+	int x, y, len, time, on, pop;
 }Bubble;
 typedef struct OBJECT {
 	int x, y;
@@ -86,63 +90,135 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	HDC hdc, memdc;
 	//HDC mem1dc, mem2dc;
 	static HBITMAP hBit1, hBit2;
+	HBRUSH hBrush, oldBrush;
 	//HBITMAP oldBit1, oldBit2;
-	static int bubble_num = 0;
+	static int bubble_num[2] = { 1,1 }, count1 = 0, count2 = 0;
 	static int x = 0, y = 0, w = 330, h = 240, mx = 0, my = 0, mw = 1600, mh = 1200;
 	static Character character[2];
-	static Bubble* bubble;
+	static Bubble bubble[14];
 	static Object obj;
 	static Item item;
 
 	switch (uMsg) {
 	case WM_CREATE:
 		SetTimer(hWnd, 1, 100, NULL);
+		// character setting
+		character[0].num_bubble = 1; character[1].num_bubble = 1;
+		character[0].speed = 1; character[1].speed = 1;
+		character[0].bubble_len = 1; character[1].bubble_len = 1;
+		character[0].x = 0; character[0].y = 0; character[1].x = 39; character[1].y = 39;
+
+		for (int i = 0; i < 14; i++)
+		{
+			bubble[i].on = 0; bubble[i].pop = 0;
+			bubble[i].time = 0;
+		}
 		hBit1 = (HBITMAP)LoadBitmap(g_hInst, MAKEINTRESOURCE(101));
 		break;
 	case WM_TIMER:
-		for (int i = 0; i < bubble_num; i++)
+	{ int pop_bubble = 0; 
+		for (int i = 0; i < 14; i++)
 		{
-			bubble[i].time += 1;
+			if (bubble[i].pop == 1) {
+				bubble[i].pop = 0;
+				pop_bubble = i;
+				InvalidateRect(hWnd, NULL, TRUE);
+			}
+			if (bubble[i].on == 1) {
+
+				bubble[i].time = (bubble[i].time + 1);
+				if (bubble[i].time % 30 == 0) {
+				if (i < 6) count1 -= 1;
+				else count2 -= 1;
+				bubble[i].on = 0; bubble[i].pop = 1;
+				bubble[i].time = 0;
+				InvalidateRect(hWnd, NULL, TRUE);
+				}
+			}
+		}
+
+		//collision
+		box CharA = { character[0].x, character[0].x + 1, character[0].y, character[0].y + 1 };
+		box CharB = { character[1].x, character[1].x + 1, character[1].y, character[1].y + 1 };
+		box B_1 = { bubble[pop_bubble].x + 0, bubble[pop_bubble].x + 1, bubble[pop_bubble].y + 0, bubble[pop_bubble].y + 1 };
+		box B_2 = { bubble[pop_bubble].x + 1, bubble[pop_bubble].x + 2, bubble[pop_bubble].y + 0, bubble[pop_bubble].y + 1 };
+		box B_3 = { bubble[pop_bubble].x - 1, bubble[pop_bubble].x + 0, bubble[pop_bubble].y + 0, bubble[pop_bubble].y + 1 };
+		box B_4 = { bubble[pop_bubble].x + 0, bubble[pop_bubble].x + 1, bubble[pop_bubble].y + 1, bubble[pop_bubble].y + 2 };
+		box B_5 = { bubble[pop_bubble].x + 0, bubble[pop_bubble].x + 1, bubble[pop_bubble].y - 1, bubble[pop_bubble].y + 0 };
+
+		if (collision(CharA, B_1) == 1) character[0].state = 1;
 		}
 		InvalidateRect(hWnd, NULL, FALSE);
 		break;
-	case WM_KEYDOWN:
+	case WM_KEYDOWN: // MOVE: wasd // BUBBLE: f  0
 		switch (wParam)
 		{
+			{
 		case VK_LEFT:
-			if (character[0].x < window_size_w) ++character[0].x;
+			if ((character[0].x) * 20 > 0) character[0].x -= character[0].speed;
 			break;
 		case VK_RIGHT:
-			if (character[0].x > 0) --character[0].x;
+			if ((character[0].x + 1) * 20 < window_size_w) character[0].x += character[0].speed;
 			break;
 		case VK_UP:
-			if (character[0].y > 0) --character[0].y;
+			if ((character[0].y) * 20 > 0) character[0].y -= character[0].speed;
 			break;
 		case VK_DOWN:
-			if (character[0].y < window_size_d) ++character[0].y;
+			if ((character[0].y + 1) * 20 < window_size_d) character[0].y += character[0].speed;
 			break;
-		case VK_LSHIFT:
-
+		case 'D':
+		case 'd':
+			if ((character[1].x + 1) * 20 < window_size_w) character[1].x += character[1].speed;
 			break;
-		case '4':
-			if (character[1].x < window_size_w) ++character[1].x;
+		case 'W':
+		case 'w':
+			if ((character[1].y) * 20 > 0) character[1].y -= character[0].speed;
 			break;
-		case '6':
-			if (character[1].x > 0) --character[1].x;
+		case 'S':
+		case 's':
+			if ((character[1].y + 1) * 20 < window_size_d) character[1].y += character[0].speed;
 			break;
-		case '8':
-			if (character[1].y > 0) --character[1].y;
+		case 'a':
+		case 'A':
+			if ((character[1].x) * 20 > 0) character[1].x -= character[1].speed;
+			break; }
+		//case VK_LSHIFT:
+		//	if (bubble_num[0] < character[0].num_bubble) {
+		//		//++bubble_num[0]; 
+		//		bubble[count1].x = character[0].x; bubble[count1].y = character[0].y;
+		//		bubble[count1].time = 0; bubble[count1].on = 1;
+		//		count1 = (count1 + 1) % character[0].num_bubble;
+		//	}
+		//	break;
+		//case VK_RSHIFT:
+		//	if (bubble_num[1] < character[1].num_bubble) {
+		//		//++bubble_num[1]; 
+		//		bubble[6+count2].x = character[1].x; bubble[6+count2].y = character[1].y;
+		//		bubble[count2].time = 0; bubble[count2].on = 1;
+		//		count2 = (count2 + 1) % character[1].num_bubble;
+		//	}
+		//	break;
+		case 'f':
+		case 'F':
+			if (bubble_num[0] > count1) {
+				//++bubble_num[0]; 
+				bubble[count1].x = character[0].x; bubble[count1].y = character[0].y;
+				bubble[count1].time = 0; bubble[count1].on = 1;
+				count1 = (count1 + 1) % (character[0].num_bubble+1);
+			}
 			break;
-		case '5':
-			if (character[1].y < window_size_d) ++character[1].y;
-			break;
-		case VK_RSHIFT:
-
+		case 0x60:
+			if (bubble_num[1] > count2) {
+				//++bubble_num[1]; 
+				bubble[6 + count2].x = character[1].x; bubble[6 + count2].y = character[1].y;
+				bubble[6 + count2].time = 0; bubble[6 + count2].on = 1;
+				count2 = (count2 + 1) % (character[1].num_bubble+1);
+			}
 			break;
 		default:
 			break;
 		}
-		InvalidateRect(hWnd, NULL, FALSE);
+		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
@@ -151,6 +227,49 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		//oldBit1 = (HBITMAP)SelectObject(memdc, hBit1);
 		SelectObject(memdc, hBit1);
 
+		hBrush = CreateSolidBrush(RGB(0, 0, 255));
+		oldBrush = (HBRUSH)SelectObject(hdc, hBrush);
+		for (int i = 0; i < bubble_num[0]; i++)
+		{
+			if (bubble[i].on == 1)
+				Rectangle(hdc, bubble[i].x * 20, bubble[i].y * 20, (bubble[i].x + 1) * 20, (bubble[i].y + 1) * 20);
+		}
+		for (int i = 0; i < bubble_num[1]; i++)
+		{
+			if (bubble[6 + i].on == 1)
+				Rectangle(hdc, bubble[6 + i].x * 20, bubble[6 + i].y * 20, (bubble[6 + i].x + 1) * 20, (bubble[6 + i].y + 1) * 20);
+		}
+		hBrush = CreateSolidBrush(RGB(0, 0, 255));
+		oldBrush = (HBRUSH)SelectObject(hdc, hBrush);
+		for (int i = 0; i < bubble_num[0]; i++)
+		{
+			if (bubble[i].pop == 1)
+			{
+				Rectangle(hdc, (bubble[i].x) * 20, (bubble[i].y) * 20, (bubble[i].x + 1) * 20, (bubble[i].y + 1) * 20);
+				Rectangle(hdc, (bubble[i].x + 1) * 20, (bubble[i].y) * 20, (bubble[i].x + 2) * 20, (bubble[i].y + 1) * 20);
+				Rectangle(hdc, (bubble[i].x - 1) * 20, (bubble[i].y) * 20, (bubble[i].x) * 20, (bubble[i].y + 1) * 20);
+				Rectangle(hdc, (bubble[i].x) * 20, (bubble[i].y + 1) * 20, (bubble[i].x + 1) * 20, (bubble[i].y + 2) * 20);
+				Rectangle(hdc, (bubble[i].x) * 20, (bubble[i].y - 1) * 20, (bubble[i].x + 1) * 20, (bubble[i].y) * 20);
+			}
+		}
+		for (int i = 0; i < bubble_num[1]; i++)
+		{
+			if (bubble[6 + i].pop == 1)
+			{
+				Rectangle(hdc, (bubble[6 + i].x) * 20, (bubble[6 + i].y) * 20, (bubble[6 + i].x + 1) * 20, (bubble[6 + i].y + 1) * 20);
+				Rectangle(hdc, (bubble[6 + i].x + 1) * 20, (bubble[6 + i].y) * 20, (bubble[6 + i].x + 2) * 20, (bubble[6 + i].y + 1) * 20);
+				Rectangle(hdc, (bubble[6 + i].x - 1) * 20, (bubble[6 + i].y) * 20, (bubble[6 + i].x) * 20, (bubble[6 + i].y + 1) * 20);
+				Rectangle(hdc, (bubble[6 + i].x) * 20, (bubble[6 + i].y + 1) * 20, (bubble[6 + i].x + 1) * 20, (bubble[6 + i].y + 2) * 20);
+				Rectangle(hdc, (bubble[6 + i].x) * 20, (bubble[6 + i].y - 1) * 20, (bubble[6 + i].x + 1) * 20, (bubble[6 + i].y) * 20);
+			}
+		}
+
+		hBrush = CreateSolidBrush(RGB(255, 0, 0));
+		oldBrush = (HBRUSH)SelectObject(hdc, hBrush);
+		Rectangle(hdc, character[0].x * 20, character[0].y * 20, (character[0].x + 1) * 20, (character[0].y + 1)* 20);
+		hBrush = CreateSolidBrush(RGB(0, 255, 0));
+		oldBrush = (HBRUSH)SelectObject(hdc, hBrush); 
+		Rectangle(hdc, character[1].x * 20, character[1].y * 20, (character[1].x + 1) * 20, (character[1].y + 1) * 20);
 
 		DeleteDC(memdc);
 		EndPaint(hWnd, &ps);
